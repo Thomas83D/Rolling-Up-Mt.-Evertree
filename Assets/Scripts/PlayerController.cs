b89f9be1ad7c94b7b5fd6968c60664e31272b3b3
Thomas83 <thomas83@sheridancollege.ca>
2026-07-18 03:13:48 -0400

    Player Movement
    Added acceleration and deceleration to player movement. More responsive to controls and will adjust variables later.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,26 +3,33 @@ using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float maxSpeed;
+    public float accelerationTime, decelerationTime;
+
+    private float acceleration, deceleration;
     private Rigidbody rb;
 
     public Transform cam;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        acceleration = maxSpeed / accelerationTime;
+        deceleration = maxSpeed / decelerationTime;
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveRight = Input.GetAxis("Horizontal") * speed;
+        float moveRight = Input.GetAxis("Horizontal") * acceleration;
 
         Vector3 camRight = cam.right;
         camRight.y = 0;
         Vector3 rightRelatve = camRight * moveRight;
         Vector3 moveDir = rightRelatve;
 
-        rb.AddForce(moveDir, ForceMode.Force);
+        moveDir += -rb.linearVelocity;
+
+        rb.AddForce(moveDir * deceleration, ForceMode.Force);
     }
 }
